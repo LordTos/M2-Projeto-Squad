d4ea352f64c910e878f26df6d666e4e76f5184f1
Orlando <opseua@gmail.com>
2022-11-05 12:10:51 -0300

    Adicionado foto de perfil

--- js/code_index_e_formulario.js
+++ js/code_index_e_formulario.js
@@ -1,8 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
-
-
+const chk = document.getElementById('chk')
 
 hamburger.addEventListener('click', () => {
   //Links
@@ -15,14 +14,43 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle("toggle");
 });
 
-const chk = document.getElementById('chk')
+// Checar modo escuro ao carregar a página // 
+function checar_modo_escuro() {
+  if (window.localStorage.getItem('modo_noturno') == "ligado") {
+    document.getElementById("ball").click()
+    alert("FOI LIGADO");
+    return "ligado";
+  }
+  else {
+    window.localStorage.setItem('modo_noturno', 'desligado');
+    alert("FOI DESLIGADO");
+    return "desligado";
+  }
+}
 
+
+// Registrar estado do modo escuro ao clicar no botão // 
 chk.addEventListener('change', () => {
+  if (window.localStorage.getItem('modo_noturno') == "ligado") {
+    window.localStorage.setItem('modo_noturno', 'desligado')
+  }
+  else{
+    window.localStorage.setItem('modo_noturno', 'ligado')
+  }
+
+});
+
+
+
+
+// Alterar modo escuro // 
+chk.addEventListener('change', () => {
+
   document.body.classList.toggle('dark')
+
 
   // ############################################ ORLANDO ############################################ //
   document.getElementById("container").classList.toggle('dark')
   // ############################################ ORLANDO ############################################ //
-
 
 });
