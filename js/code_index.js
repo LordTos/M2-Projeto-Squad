630d619c4bb38f79c21ac4acde4df986a63d79c7
Thiago de Oliveira Sant' Anna <113534772+LordTos@users.noreply.github.com>
2022-11-06 21:02:56 -0300

    Ajustes de Links

--- js/code_index.js
+++ js/code_index.js
@@ -1,20 +1,55 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+//FUNCÃO PARA SCROLL EM LINKS INTERNOS NA PAGINA *PAROU DE FUNCIONAR DPS DO MERGER....*
+const menuItems = document.querySelectorAll('.menu-lt a[href^="#"]');
 
-hamburger.addEventListener('click', ()=>{
-   //Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
+function getScrollTopByHref(element) {
+	const id = element.getAttribute('href');
+	return document.querySelector(id).offsetTop;
+}
 
-    //Animation
-    hamburger.classList.toggle("toggle");
+function scrollToPosition(to) {
+  
+  smoothScrollTo(0, to);
+}
+
+function scrollToIdOnClick(event) {
+	event.preventDefault();
+	const to = getScrollTopByHref(event.currentTarget)- 110;
+	scrollToPosition(to);
+}
+
+menuItems.forEach(item => {
+	item.addEventListener('click', scrollToIdOnClick);
 });
 
-const chk = document.getElementById('chk')
 
-chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-});
+/**
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int) endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== 'undefined' ? duration : 400;
+
+  
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60); // 60 fps
+};
